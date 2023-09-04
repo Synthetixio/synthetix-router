d428e255feea51cb8862811d4152241e40a736fa
Matías <mjlescano@protonmail.com>
2023-09-04 17:09:29 +0100

    add metadata when compiling

--- src/compile.ts
+++ src/compile.ts
@@ -1,4 +1,24 @@
 import solc from 'solc';
+import { JsonFragment } from '@ethersproject/abi';
+
+interface CompileResult {
+  abi: JsonFragment[];
+  metadata: string;
+  solcVersion: string;
+  assembly: string;
+  bytecode: string;
+  deployedBytecode: string;
+  gasEstimates: {
+    creation: {
+      codeDepositCost: string;
+      executionCost: string;
+      totalCost: string;
+    };
+    external: {
+      '': string;
+    };
+  };
+}
 
 export function getCompileInput(contractName: string, sourceCode: string) {
   return {
@@ -23,6 +43,16 @@ export async function compileContract(contractName: string, sourceCode: string) 
   const input = getCompileInput(contractName, sourceCode);
 
   const solResult = JSON.parse(await solc.compile(JSON.stringify(input)));
+  const info = solResult.contracts[`${contractName}.sol`][contractName];
+  const metadata = JSON.parse(info.metadata);
 
-  return solResult.contracts[`${contractName}.sol`][contractName];
+  return {
+    abi: info.abi,
+    metadata: info.metadata,
+    solcVersion: metadata.compiler.version,
+    assembly: info.evm.assembly,
+    bytecode: info.evm.bytecode.object,
+    deployedBytecode: info.evm.deployedBytecode.object,
+    gasEstimates: info.evm.gasEstimates,
+  } as CompileResult;
 }
